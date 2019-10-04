bb5709dd1317409893905849300c6ccb0ead0cc4
everson.borges <everson.borges@axxiom.com.br>
2019-10-04 17:05:23 -0300

    Mapeamento Classe EscolaBiblica

--- src/main/java/com/sigesigapi/sigesig/model/EscolaBiblica.java
+++ src/main/java/com/sigesigapi/sigesig/model/EscolaBiblica.java
@@ -1,5 +1,35 @@
 package com.sigesigapi.sigesig.model;
 
+import java.util.Calendar;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.sigesigapi.sigesig.enums.Status;
+import com.sigesigapi.sigesig.enums.Turmas;
+
+@Entity
+@Table(name = "escola_biblica")
 public class EscolaBiblica {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idEscolaBiblica;
+	
+	@Enumerated(EnumType.STRING)
+	private Turmas turma;
+	
+
+	private Membro professor;
+	private Calendar dtInicio;
+	private Status tipo;
+	private Calendar dtNasc;
+	private Templo templo;
+	
+	
 }
